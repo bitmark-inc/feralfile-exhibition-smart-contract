986293da4c456dde05d18f90dba9172845f8eabd
Jim Yeh <jim@bitmark.com>
2021-11-14 11:49:50 +0800

    Update the naming for function arguments

--- contracts/FeralfileArtworkV2.sol
+++ contracts/FeralfileArtworkV2.sol
@@ -49,30 +49,30 @@ contract FeralfileExhibitionV2 is ERC721Enumerable, Authorizable, IERC2981 {
     mapping(string => bool) internal registeredIPFSCIDs; // ipfsCID => bool
 
     constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _maxEditionPerArtwork,
-        uint256 _secondarySaleRoyaltyBPS,
-        address _royaltyPayoutAddress,
+        string memory name_,
+        string memory symbol_,
+        uint256 maxEditionPerArtwork_,
+        uint256 secondarySaleRoyaltyBPS_,
+        address royaltyPayoutAddress_,
         string memory contractURI_,
         string memory tokenBaseURI_
-    ) ERC721(_name, _symbol) {
+    ) ERC721(name_, symbol_) {
         require(
-            _maxEditionPerArtwork > 0,
+            maxEditionPerArtwork_ > 0,
             "maxEdition of each artwork in an exhibition needs to be greater than zero"
         );
         require(
-            _secondarySaleRoyaltyBPS <= MAX_ROYALITY_BPS,
+            secondarySaleRoyaltyBPS_ <= MAX_ROYALITY_BPS,
             "royalty BPS for secondary sales can not be greater than the maximum royalty BPS"
         );
         require(
-            _royaltyPayoutAddress != address(0),
+            royaltyPayoutAddress_ != address(0),
             "invalid royalty payout address"
         );
 
-        maxEditionPerArtwork = _maxEditionPerArtwork;
-        secondarySaleRoyaltyBPS = _secondarySaleRoyaltyBPS;
-        royaltyPayoutAddress = _royaltyPayoutAddress;
+        maxEditionPerArtwork = maxEditionPerArtwork_;
+        secondarySaleRoyaltyBPS = secondarySaleRoyaltyBPS_;
+        royaltyPayoutAddress = royaltyPayoutAddress_;
         _contractURI = contractURI_;
         _tokenBaseURI = tokenBaseURI_;
     }
@@ -90,47 +90,44 @@ contract FeralfileExhibitionV2 is ERC721Enumerable, Authorizable, IERC2981 {
     }
 
     /// @notice Call to create an artwork in the exhibition
-    /// @param _fingerprint - the fingerprint of an artwork
-    /// @param _title - the title of an artwork
-    /// @param _artistName - the artist of an artwork
-    /// @param _editionSize - the maximum edition size of an artwork
+    /// @param fingerprint - the fingerprint of an artwork
+    /// @param title - the title of an artwork
+    /// @param artistName - the artist of an artwork
+    /// @param editionSize - the maximum edition size of an artwork
     function createArtwork(
-        string memory _fingerprint,
-        string memory _title,
-        string memory _artistName,
-        uint256 _editionSize
+        string memory fingerprint,
+        string memory title,
+        string memory artistName,
+        uint256 editionSize
     ) external onlyAuthorized {
-        require(bytes(_title).length != 0, "title can not be empty");
-        require(bytes(_artistName).length != 0, "artist can not be empty");
+        require(bytes(title).length != 0, "title can not be empty");
+        require(bytes(artistName).length != 0, "artist can not be empty");
+        require(bytes(fingerprint).length != 0, "fingerprint can not be empty");
+        require(editionSize > 0, "edition size needs to be at least 1");
         require(
-            bytes(_fingerprint).length != 0,
-            "fingerprint can not be empty"
-        );
-        require(_editionSize > 0, "edition size needs to be at least 1");
-        require(
-            _editionSize <= maxEditionPerArtwork,
+            editionSize <= maxEditionPerArtwork,
             "artwork edition size exceeds the maximum edition size of the exhibition"
         );
 
-        uint256 _artworkID = uint256(keccak256(abi.encode(_fingerprint)));
+        uint256 artworkID = uint256(keccak256(abi.encode(fingerprint)));
 
         /// @notice make sure the artwork have not been registered
         require(
-            bytes(artworks[_artworkID].fingerprint).length == 0,
+            bytes(artworks[artworkID].fingerprint).length == 0,
             "an artwork with the same fingerprint has already registered"
         );
 
-        Artwork memory _artwork = Artwork(
-            _fingerprint,
-            _title,
-            _artistName,
-            _editionSize
+        Artwork memory artwork = Artwork(
+            fingerprint,
+            title,
+            artistName,
+            editionSize
         );
 
-        _allArtworks.push(_artworkID);
-        artworks[_artworkID] = _artwork;
+        _allArtworks.push(artworkID);
+        artworks[artworkID] = artwork;
 
-        emit NewArtwork(_artworkID);
+        emit NewArtwork(artworkID);
     }
 
     /// @notice Return a count of artworks registered in this exhibition
@@ -153,69 +150,73 @@ contract FeralfileExhibitionV2 is ERC721Enumerable, Authorizable, IERC2981 {
     }
 
     /// @notice Swap an existent artwork from bitmark to ERC721
-    /// @param _artworkID - the artwork id where the new edition is referenced to
-    /// @param _bitmarkID - the bitmark id of artwork edition before swapped
-    /// @param _editionNumber - the edition number of the artwork edition
-    /// @param _owner - the owner address of the new minted token
-    /// @param _ipfsCID - the IPFS cid for the new token
+    /// @param artworkID - the artwork id where the new edition is referenced to
+    /// @param bitmarkID - the bitmark id of artwork edition before swapped
+    /// @param editionNumber - the edition number of the artwork edition
+    /// @param owner - the owner address of the new minted token
+    /// @param ipfsCID - the IPFS cid for the new token
     function swapArtworkFromBitmark(
-        uint256 _artworkID,
-        uint256 _bitmarkID,
-        uint256 _editionNumber,
-        address _owner,
-        string memory _ipfsCID
+        uint256 artworkID,
+        uint256 bitmarkID,
+        uint256 editionNumber,
+        address owner,
+        string memory ipfsCID
     ) external onlyAuthorized {
         /// @notice the edition size is not set implies the artwork is not created
-        require(artworks[_artworkID].editionSize > 0, "artwork is not found");
-        /// @notice The range of _editionNumber should be between 0 (AP) ~ artwork.editionSize
+        require(artworks[artworkID].editionSize > 0, "artwork is not found");
+        /// @notice The range of editionNumber should be between 0 (AP) ~ artwork.editionSize
         require(
-            _editionNumber <= artworks[_artworkID].editionSize,
+            editionNumber <= artworks[artworkID].editionSize,
             "edition number exceed the edition size of the artwork"
         );
-        require(_owner != address(0), "invalid owner address");
-        require(!registeredBitmarks[_bitmarkID], "bitmark id has registered");
-        require(!registeredIPFSCIDs[_ipfsCID], "ipfs id has registered");
+        require(owner != address(0), "invalid owner address");
+        require(!registeredBitmarks[bitmarkID], "bitmark id has registered");
+        require(!registeredIPFSCIDs[ipfsCID], "ipfs id has registered");
 
-        uint256 editionID = _artworkID + _editionNumber;
+        uint256 editionID = artworkID + editionNumber;
         require(
             artworkEditions[editionID].editionID == 0,
             "the edition is existent"
         );
 
-        ArtworkEdition memory edition = ArtworkEdition(editionID, _ipfsCID);
+        ArtworkEdition memory edition = ArtworkEdition(editionID, ipfsCID);
 
         artworkEditions[editionID] = edition;
-        allArtworkEditions[_artworkID].push(editionID);
+        allArtworkEditions[artworkID].push(editionID);
 
-        registeredBitmarks[_bitmarkID] = true;
-        registeredIPFSCIDs[_ipfsCID] = true;
+        registeredBitmarks[bitmarkID] = true;
+        registeredIPFSCIDs[ipfsCID] = true;
 
-        _safeMint(_owner, editionID);
-        emit NewArtworkEdition(_owner, _artworkID, editionID);
+        _safeMint(owner, editionID);
+        emit NewArtworkEdition(owner, artworkID, editionID);
     }
 
     /// @notice Update the IPFS cid of an edition to a new value
-    function updateArtworkEditionIPFSCid(
-        uint256 _tokenId,
-        string memory _ipfsCID
-    ) external onlyAuthorized {
-        require(_exists(_tokenId), "artwork edition is not found");
-        require(!registeredIPFSCIDs[_ipfsCID], "ipfs id has registered");
+    function updateArtworkEditionIPFSCid(uint256 tokenId, string memory ipfsCID)
+        external
+        onlyAuthorized
+    {
+        require(_exists(tokenId), "artwork edition is not found");
+        require(!registeredIPFSCIDs[ipfsCID], "ipfs id has registered");
 
-        ArtworkEdition storage edition = artworkEditions[_tokenId];
+        ArtworkEdition storage edition = artworkEditions[tokenId];
         delete registeredIPFSCIDs[edition.ipfsCID];
-        registeredIPFSCIDs[_ipfsCID] = true;
-        edition.ipfsCID = _ipfsCID;
+        registeredIPFSCIDs[ipfsCID] = true;
+        edition.ipfsCID = ipfsCID;
     }
 
     /// @notice setRoyaltyPayoutAddress assigns a payout address so
     //          that we can split the royalty.
-    function setRoyaltyPayoutAddress(address payoutAddress)
+    /// @param royaltyPayoutAddress_ - the new royalty payout address
+    function setRoyaltyPayoutAddress(address royaltyPayoutAddress_)
         external
         onlyAuthorized
     {
-        require(payoutAddress != address(0), "invalid royalty payout address");
-        royaltyPayoutAddress = payoutAddress;
+        require(
+            royaltyPayoutAddress_ != address(0),
+            "invalid royalty payout address"
+        );
+        royaltyPayoutAddress = royaltyPayoutAddress_;
     }
 
     /// @notice Return the edition counts for an artwork
@@ -277,25 +278,25 @@ contract FeralfileExhibitionV2 is ERC721Enumerable, Authorizable, IERC2981 {
 
     /// @notice Called with the sale price to determine how much royalty
     //          is owed and to whom.
-    /// @param _tokenId - the NFT asset queried for royalty information
-    /// @param _salePrice - the sale price of the NFT asset specified by _tokenId
+    /// @param tokenId - the NFT asset queried for royalty information
+    /// @param salePrice - the sale price of the NFT asset specified by tokenId
     /// @return receiver - address of who should be sent the royalty payment
-    /// @return royaltyAmount - the royalty payment amount for _salePrice
-    function royaltyInfo(uint256 _tokenId, uint256 _salePrice)
+    /// @return royaltyAmount - the royalty payment amount for salePrice
+    function royaltyInfo(uint256 tokenId, uint256 salePrice)
         external
         view
         override
         returns (address receiver, uint256 royaltyAmount)
     {
         require(
-            _exists(_tokenId),
+            _exists(tokenId),
             "ERC2981: query royalty info for nonexistent token"
         );
 
         receiver = royaltyPayoutAddress;
 
         royaltyAmount =
-            (_salePrice * secondarySaleRoyaltyBPS) /
+            (salePrice * secondarySaleRoyaltyBPS) /
             MAX_ROYALITY_BPS;
     }
 
