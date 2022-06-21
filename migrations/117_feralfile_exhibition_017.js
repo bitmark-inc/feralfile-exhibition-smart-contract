c70a331f095b8a75d9426090e5ae84f7dd1a05b6
Jim Yeh <jim@bitmark.com>
2022-06-21 14:25:00 +0800

    Add deployment information for exhibition 14 - 17

---
+++ migrations/117_feralfile_exhibition_017.js
@@ -0,0 +1,18 @@
+var FeralfileExhibitionV2 = artifacts.require("FeralfileExhibitionV2");
+
+const argv = require('minimist')(process.argv.slice(2), {
+  string: ['exhibition_curator']
+});
+
+module.exports = function (deployer) {
+  let exhibition_name = argv.exhibition_name || "Feral File — WETWARE";
+  let exhibition_symbol = argv.exhibition_symbol || "FF017";
+  let exhibition_edition_size = argv.exhibition_edition_size || 80;
+  let exhibition_secondary_sale_royalty_bps = argv.exhibition_secondary_sale_royalty_bps || 1500;
+  let exhibition_royalty_payout_address = argv.exhibition_royalty_payout_address || "0xf6d099037Df564a32237705e5005F4450b0EB6c3";
+
+  deployer.deploy(FeralfileExhibitionV2, exhibition_name, exhibition_symbol,
+    exhibition_edition_size, exhibition_secondary_sale_royalty_bps, exhibition_royalty_payout_address,
+    "https://ipfs.bitmark.com/ipfs/QmUry9URE9UBQMbqrrcsBe6pqi6851HNJhBgThGZM2urBR",
+    "https://ipfs.bitmark.com/ipfs/");
+};
