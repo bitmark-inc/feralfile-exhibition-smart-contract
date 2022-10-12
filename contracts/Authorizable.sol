2809578ee564d426b0c56540823ac509fd78bae2
Tien Ngo <tien@bitmark.com>
2022-10-12 09:18:48 +0700

    Removed istrustee and make trustees public

--- contracts/Authorizable.sol
+++ contracts/Authorizable.sol
@@ -4,17 +4,13 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Authorizable is Ownable {
-    mapping(address => bool) private trustees;
+    mapping(address => bool) public trustees;
 
     constructor() {}
 
     modifier onlyAuthorized() {
         require(trustees[msg.sender] || msg.sender == owner());
         _;
-    }
-
-    function isTrustee(address _trustee) public view returns (bool) {
-        return trustees[_trustee];
     }
 
     function addTrustee(address _trustee) public onlyOwner {
