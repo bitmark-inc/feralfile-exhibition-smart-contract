ebfb4a28afd36d4f7af84a119b705040b9342caa
Tien Ngo <tien@bitmark.com>
2022-10-27 23:04:30 +0700

    Added AE, PP amount to creating new artwork and burn edition method

--- migrations/200_feralfile_exhibition_v3.js
+++ migrations/200_feralfile_exhibition_v3.js
@@ -7,19 +7,25 @@ const argv = require('minimist')(process.argv.slice(2), {
 module.exports = function (deployer) {
   let exhibition_name = argv.exhibition_name || 'Feral File V3';
   let exhibition_symbol = argv.exhibition_symbol || 'FFDV2001';
+  let code_version = argv.code_version || 'V3';
   let exhibition_royalty_payout_address =
     argv.exhibition_royalty_payout_address ||
     '0x2760869A50D48F1C67253C4461c0A6f9e1440Cac';
   let exhibition_secondary_sale_royalty_bps =
     argv.exhibition_secondary_sale_royalty_bps || 1000;
+  let burnable = argv.burnable || true;
+  let bridgeable = argv.bridgeable || true;
 
   deployer.deploy(
     FeralfileExhibitionV3,
     exhibition_name,
     exhibition_symbol,
+    code_version,
     exhibition_secondary_sale_royalty_bps,
     exhibition_royalty_payout_address,
     'https://ipfs.bitmark.com/ipfs/QmaptARVxNSP36PQai5oiCPqbrATvpydcJ8SPx6T6Yp1CZ',
-    'https://ipfs.bitmark.com/ipfs/'
+    'https://ipfs.bitmark.com/ipfs/',
+    burnable,
+    bridgeable
   );
 };
