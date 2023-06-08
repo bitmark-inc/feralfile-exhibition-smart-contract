ff072f168af46a670eae2bee6354904cc5d9bf9f
lpopo0856 <lpopo0856@gmail.com>
2023-06-08 17:39:51 +0800

    add more test case to vault

--- test/feralfile_vault.js
+++ test/feralfile_vault.js
@@ -21,11 +21,68 @@ contract('FeralfileVault', async (accounts) => {
     });
 
     it("check payForSale", async function () {
+        // Generate signature
+        const expiryTime = (new Date().getTime() / 1000 + 300).toFixed(0);
+        const saleData = [
+            BigInt(0.1 * 1e18).toString(),
+            BigInt(0.02 * 1e18).toString(),
+            expiryTime,
+            accounts[2],
+            [1000000, 1000001, 2000000, 2000001, 2000002],
+            [
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+            ],
+            true,
+        ];
+        const signParams = web3.eth.abi.encodeParameters(
+            [
+                "uint",
+                "address",
+                "tuple(uint256,uint256,uint256,address,uint256[],tuple(address,uint256)[][],bool)",
+            ],
+            [
+                BigInt(await web3.eth.getChainId()).toString(),
+                accounts[0],
+                saleData,
+            ]
+        );
+        const hash = web3.utils.keccak256(signParams);
+        var sig = await web3.eth.sign(hash, this.signer);
+        sig = sig.substr(2);
+        const r = "0x" + sig.slice(0, 64);
+        const s = "0x" + sig.slice(64, 128);
+        const v = web3.utils.toDecimal("0x" + sig.slice(128, 130)) + 27;
+
+        // pay with invalid rsv
         try {
-            // Generate signature
-            const expiryTime = (new Date().getTime() / 1000 + 300).toFixed(0);
-            const saleData = [
-                BigInt(0.1 * 1e18).toString(),
+            await this.vault.payForSale("0x00", "0x00", "0x1c", saleData);
+        } catch (error) {
+            assert.ok(error.message.includes("ECDSA: invalid signature"));
+        }
+
+        // pay with wrong saleData
+        try {
+            const wrongSaleData = [
+                BigInt(0.3 * 1e18).toString(), // wrong price amount
                 BigInt(0.02 * 1e18).toString(),
                 expiryTime,
                 accounts[2],
@@ -54,30 +111,142 @@ contract('FeralfileVault', async (accounts) => {
                 ],
                 true,
             ];
-            const signParams = web3.eth.abi.encodeParameters(
-                [
-                    "uint",
-                    "address",
-                    "tuple(uint256,uint256,uint256,address,uint256[],tuple(address,uint256)[][],bool)",
-                ],
-                [
-                    BigInt(await web3.eth.getChainId()).toString(),
-                    accounts[0],
-                    saleData,
-                ]
-            );
-            const hash = web3.utils.keccak256(signParams);
-            var sig = await web3.eth.sign(hash, this.signer);
-            sig = sig.substr(2);
-            const r = "0x" + sig.slice(0, 64);
-            const s = "0x" + sig.slice(64, 128);
-            const v = web3.utils.toDecimal("0x" + sig.slice(128, 130)) + 27;
+            await this.vault.payForSale(r, s, v, wrongSaleData);
+        } catch (error) {
+            assert.ok(error.message.includes("FeralfileVault: invalid signature"));
+        }
+
+        // pay successfully
+        try {
             await this.vault.payForSale(r, s, v, saleData);
             const vaultBalanceAfter = await web3.eth.getBalance(this.vault.address);
             assert.equal(vaultBalanceAfter, 0.4 * 1e18);
         } catch (error) {
             console.log(error)
             assert.fail();
+        }
+
+        // pay again, try to drain contract fund
+        try {
+            await this.vault.payForSale(r, s, v, saleData);
+        } catch (error) {
+            assert.ok(error.message.includes("FeralfileVault: paid sale"));
+        }
+    });
+
+    it("call payForSale using crypto sale signature", async function () {
+        // Generate signature
+        const expiryTime = (new Date().getTime() / 1000 + 300).toFixed(0);
+        const saleData = [
+            BigInt(0.1 * 1e18).toString(),
+            BigInt(0.02 * 1e18).toString(),
+            expiryTime,
+            accounts[2],
+            [1000000, 1000001, 2000000, 2000001, 2000002],
+            [
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+            ],
+            false,
+        ];
+        const signParams = web3.eth.abi.encodeParameters(
+            [
+                "uint",
+                "address",
+                "tuple(uint256,uint256,uint256,address,uint256[],tuple(address,uint256)[][],bool)",
+            ],
+            [
+                BigInt(await web3.eth.getChainId()).toString(),
+                accounts[0],
+                saleData,
+            ]
+        );
+        const hash = web3.utils.keccak256(signParams);
+        var sig = await web3.eth.sign(hash, this.signer);
+        sig = sig.substr(2);
+        const r = "0x" + sig.slice(0, 64);
+        const s = "0x" + sig.slice(64, 128);
+        const v = web3.utils.toDecimal("0x" + sig.slice(128, 130)) + 27;
+        try {
+            await this.vault.payForSale(r, s, v, saleData);
+        } catch (error) {
+            assert.ok(error.message.includes("FeralfileVault: not pay by vault"));
+        }
+    });
+
+    it("call payForSale with more fund requested than the contract have", async function () {
+        // Generate signature
+        const expiryTime = (new Date().getTime() / 1000 + 300).toFixed(0);
+        const saleData = [
+            BigInt(10 * 1e18).toString(),
+            BigInt(0.02 * 1e18).toString(),
+            expiryTime,
+            accounts[2],
+            [1000000, 1000001, 2000000, 2000001, 2000002],
+            [
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+                [
+                    [accounts[3], 8000],
+                    [accounts[4], 2000],
+                ],
+            ],
+            true,
+        ];
+        const signParams = web3.eth.abi.encodeParameters(
+            [
+                "uint",
+                "address",
+                "tuple(uint256,uint256,uint256,address,uint256[],tuple(address,uint256)[][],bool)",
+            ],
+            [
+                BigInt(await web3.eth.getChainId()).toString(),
+                accounts[0],
+                saleData,
+            ]
+        );
+        const hash = web3.utils.keccak256(signParams);
+        var sig = await web3.eth.sign(hash, this.signer);
+        sig = sig.substr(2);
+        const r = "0x" + sig.slice(0, 64);
+        const s = "0x" + sig.slice(64, 128);
+        const v = web3.utils.toDecimal("0x" + sig.slice(128, 130)) + 27;
+        try {
+            await this.vault.payForSale(r, s, v, saleData);
+        } catch (error) {
+            assert.ok(error.message.includes("FeralfileVault: insufficient balance"));
         }
     });
 
